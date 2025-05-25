07255a813b987d7c4c0cca0116fa8aa46f3bb9c9
pkg1997 <46337515+pkg1997@users.noreply.github.com>
2025-05-25 15:49:12 +0530

    Add files via upload

---
+++ Math Function Assignment.py
@@ -0,0 +1,9 @@
+from math import *
+num = int(input("Enter a number: "))
+result1 = sqrt(num)
+result2 = log(num)
+result3 = sin(num)
+
+print("Square Root:", result1)
+print("Logarithm:", result2)
+print("Sin:" ,result3)
